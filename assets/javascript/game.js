3ee27b1d0efb81c173617d0314c5c74c62e18656
Hyejin Kim <cshjkin@gmail.com>
2019-11-02 23:12:56 -0700

    found debug and edit spelling mistakes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,14 @@ console.log("See this to try if this works")
 
 //jquery for random number playing 
 $(document).ready(function(){
-    var random = Math.floor(Math.random() * 101 + 19);
+    var random = Math.floor(Math.random()*101+19);
     //The random number shown at the start of the game should be between 19 - 120.
     $("#randomNumber").text(random);
     // need to show random number first 
-    var num1= Math.floor(Math.random() *11+1)
-    var num2= Math.floor(Math.random() * 11+1)
-    var num3= Math.floor(Math.random () *11+1)
-    var num4= Math.floor(Math.random() *11+1)
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
     //also set up random number for each jewels.Each crystal should have a random hidden value between 1 - 12.
 
     var userTotal= 0;
@@ -19,14 +19,14 @@ $(document).ready(function(){
     $("#numberWins").text(wins);
     $("#numberLosses").text(losses);
     
-    function reset (){
-        random= Math.floor(Math.random() * 101+19);
-        console.log (random)
-        $("#randonNumber").text(random);
-        num1= Math.floor(Math.random() * 11+1);
-        num2= Math.floor(Math.random() *11+1);
-        num3= Math.floor (Math.random () *11+1);
-        num4= Math.floor (Math.random() *11+1);
+    function reset() {
+        random= Math.floor(Math.random()*101+19);
+        console.log(random);
+        $("#randomNumber").text(random);
+        num1= Math.floor(Math.random()*11+1);
+        num2= Math.floor(Math.random()*11+1);
+        num3= Math.floor(Math.random()*11+1);
+        num4= Math.floor(Math.random()*11+1);
         userTotal =0;
         $("#finalTotal").text(userTotal);
     }
