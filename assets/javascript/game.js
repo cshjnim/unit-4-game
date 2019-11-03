327dd71c089cace52ccefc41cc78da772ce52759
Hyejin Kim <cshjkin@gmail.com>
2019-11-02 22:09:40 -0700

    play to check debugging and update the jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
     //The random number shown at the start of the game should be between 19 - 120.
     $("#randomNumber").text(random);
     // need to show random number first 
-    var blue= Math.floor (Math.random() *11+1);
-    var green= Math.floor (Math.random() * 11+1);
-    var red= Math.floor (Math.random () *11+1);
-    var yellow= Math.floor (Math.random() *11+1);
+    var num1= Math.floor(Math.random() *11+1)
+    var num2= Math.floor(Math.random() * 11+1)
+    var num3= Math.floor(Math.random () *11+1)
+    var num4= Math.floor(Math.random() *11+1)
     //also set up random number for each jewels.Each crystal should have a random hidden value between 1 - 12.
 
     var userTotal= 0;
@@ -23,92 +23,76 @@ $(document).ready(function(){
         random= Math.floor(Math.random() * 101+19);
         console.log (random)
         $("#randonNumber").text(random);
-        blue= Math.floor(Math.random() * 11+1);
-        green= Math.floor(Math.random() *11+1);
-        red= Math.floor (Math.random () *11+1);
-        yellow= Math.floor (Math.random() *11+1);
+        num1= Math.floor(Math.random() * 11+1);
+        num2= Math.floor(Math.random() *11+1);
+        num3= Math.floor (Math.random () *11+1);
+        num4= Math.floor (Math.random() *11+1);
         userTotal =0;
         $("#finalTotal").text(userTotal);
     }
 
-    function wins(){
+    function hooray(){
         alert("You won!");
         wins++;
         $("#numberWins").text(wins);
         reset();
     }
 
-    function losses(){
+    function eeewww(){
         alert("You lose!");
         losses++;
         $("#numberLosses").text(losses);
         reset()
     }
-});
+
  
 // for Jewel's click 
 $("#blue").on("click", function() {
-    userTotal = userTotal +blue;
+    userTotal = userTotal +num1;
     console.log("New userTotal = " +userTotal);
     $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-        wins();
+    if (userTotal === random) {
+        hooray();
     }
-    else if (userTotal > random) {
-        losses();
+    else if (userTotal> random) {
+        eeewww();
     }
-
-})
-$("#blue").on("click", function() {
-    userTotal = userTotal +blue;
-    console.log("New userTotal = " +userTotal);
-    $("#finalTotal").text(userTotal);
-
-    if (userTotal == random) {
-        wins();
-    }
-    else if (userTotal > random) {
-        losses();
-    }
-
 })
 $("#green").on("click", function() {
-    userTotal = userTotal +green;
+    userTotal = userTotal +num2;
     console.log("New userTotal = " +userTotal);
     $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-        wins();
+    if (userTotal === random) {
+        hooray();
     }
-    else if (userTotal > random) {
-        losses();
+    else if (userTotal> random) {
+        eeewww();
     }
-
 })
 $("#red").on("click", function() {
-    userTotal = userTotal +red;
+    userTotal = userTotal +num3;
     console.log("New userTotal = " +userTotal);
     $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-        wins();
+    if (userTotal === random) {
+        hooray();
     }
-    else if (userTotal > random) {
-        losses();
+    else if (userTotal> random) {
+        eeewww();
     }
-
 })
 $("#yellow").on("click", function() {
-    userTotal = userTotal +yellow;
+    userTotal = userTotal +num4;
     console.log("New userTotal = " +userTotal);
     $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-        wins();
+    if (userTotal === random) {
+        hooray();
     }
-    else if (userTotal > random) {
-        losses();
+    else if (userTotal> random) {
+        eeewww();
     }
-
 })
+});
